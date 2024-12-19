6e2a556bf240cb8674caab9b662f7ebfe368ca9d
Matthew Khoimov <matthewkhoimov@yandex.ru>
2024-12-19 20:05:47 +0200

    v0.3

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-// @todo: Темплейт карточки
-const cardTemplate = document.querySelector("#card-template").textContent;
-// @todo: DOM узлы
-const cardsList = document.querySelector(".places__list")
-// @todo: Функция создания карточки
-fucntion 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './pages/index.css';
-import { addCard, removeCard, addInitialCards, initialCards } from './scripts/cards.js';
-import { launchEditPopup, launchAddCardPopup } from './scripts/edit_info.js'
-
-addInitialCards(initialCards);
-launchEditPopup();
-launchAddCardPopup();
-
